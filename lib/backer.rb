aa74b05e50553b12ad680e55a5cd107afaad0237
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-27 17:28:22 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
 class Backer
-
-attr_reader :backed_projects, :name
-
-def initialize(name)
-  @backed_projects = []
-  @name = name
-end
-
-def back_project(project)
-  @backed_projects << project
-end
+  
+  attr_reader :backed_projects, :name
+  
+  def initialize(name)
+    @backed_projects = []
+    @name = name
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+  end
 
 end
